306c56f2cd33e2e417570645e55aa7d8ab7a2f0a
misat1505 <misat2002@gmail.com>
2024-10-30 13:21:44 +0100

    configured shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
-    <div className="font-semibold text-lg text-slate-500">Tailwind header</div>
+    <>
+      <div className="font-semibold text-lg text-slate-500">
+        Tailwind header
+      </div>
+      <Button className="font-semibold">Shadcn</Button>
+    </>
   );
 };
 
