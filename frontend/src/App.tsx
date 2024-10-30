b10605ce568face5a768414285651fe4da1cc94e
misat1505 <misat2002@gmail.com>
2024-10-30 13:32:28 +0100

    configured prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 const App = () => {
   return (
     <>
-      <div className="font-semibold text-lg text-slate-500">
+      <div className="text-lg font-semibold text-slate-500">
         Tailwind header
       </div>
       <Button className="font-semibold">Shadcn</Button>
