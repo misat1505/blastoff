18fe6f5fa39f21e817a70db4d3e32a72fe7dc720
misat1505 <misat2002@gmail.com>
2024-11-05 12:45:03 +0100

    react router setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
-import { Button } from "./components/ui/button";
+import { Suspense } from "react";
+import { routes } from "./lib/routes";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Loading from "./components/Loading";
 
 const App = () => {
   return (
-    <>
-      <div className="text-lg font-semibold text-slate-500">
-        Tailwind header
-      </div>
-      <Button className="font-semibold">Shadcn</Button>
-    </>
+    <Router>
+      <Routes>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={
+              <Suspense fallback={<Loading />}>
+                <route.component />
+              </Suspense>
+            }
+          />
+        ))}
+      </Routes>
+    </Router>
   );
 };
 
