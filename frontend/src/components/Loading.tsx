5521679b40f6a44017fb45799d23a640162ed4d0
misat1505 <misat2002@gmail.com>
2024-11-13 13:39:18 +0100

    loading component uses react spinner

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,5 +1,16 @@
-const Loading = () => {
-  return <div>loading...</div>;
+import { BarLoader } from "react-spinners";
+
+type LoadingProps = {
+  text?: string;
+};
+
+const Loading = ({ text }: LoadingProps) => {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <BarLoader color="#de670d" />
+      {text && <p>{text}</p>}
+    </div>
+  );
 };
 
 export default Loading;
