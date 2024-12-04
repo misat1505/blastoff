5ebf96d5d0075cdc1ae39fe7b85a79712fc10d57
misat1505 <misat2002@gmail.com>
2024-12-04 13:10:17 +0100

    launch page simulates fetching data from backend

--- frontend/src/pages/Launch.tsx
+++ frontend/src/pages/Launch.tsx
@@ -1,5 +1,61 @@
+import { MissionService } from "../services/MissionService";
+import NotFound from "../components/NotFound";
+import { Mission } from "../types/Mission";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { queryKeysBuilder } from "../utils/queryKeysBuilder";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+
 const LaunchPage = () => {
-  return <div>launch page</div>;
+  const { launchId } = useParams();
+
+  if (!launchId)
+    return (
+      <NotFound
+        title="Launch not found"
+        text="Launch id has to be provided. Check your URL."
+      />
+    );
+
+  return <LaunchPageContent id={launchId} />;
 };
 
 export default LaunchPage;
+
+type LaunchPageContentProps = {
+  id: Mission["id"];
+};
+
+const LaunchPageContent = ({ id }: LaunchPageContentProps) => {
+  const {
+    data: launch,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery({
+    queryFn: () => MissionService.getMissionById(id),
+    queryKey: queryKeysBuilder.launch(id),
+  });
+
+  if (isLoading) return <Loading />;
+
+  if (error)
+    return (
+      <Error
+        title="Error fetching launch"
+        description={JSON.stringify(error)}
+        handleRefresh={refetch}
+      />
+    );
+
+  if (!launch)
+    return (
+      <NotFound
+        title="Launch not found"
+        text="Launch of given ID doesn't exist."
+      />
+    );
+
+  return <div>{JSON.stringify(launch)}</div>;
+};
--- frontend/src/utils/queryKeysBuilder.ts
+++ frontend/src/utils/queryKeysBuilder.ts
@@ -1,6 +1,8 @@
+import { Mission } from "@/types/Mission";
 import { Rocket } from "../types/Rocket";
 
 export const queryKeysBuilder = {
   upcomingLaunches: () => ["upcoming-launches"] as const,
   rocket: (id: Rocket["id"]) => ["rocket", { id }] as const,
+  launch: (id: Mission["id"]) => ["launch", { id }] as const,
 };
