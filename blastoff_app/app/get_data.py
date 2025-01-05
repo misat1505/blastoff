01e3237ba4b14354a95405b6dc6f5308bd9497c2
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-05 16:11:52 +0100

    added scheduled task to get data from api

---
+++ blastoff_app/app/get_data.py
@@ -0,0 +1,55 @@
+import asyncio
+import datetime
+
+from app.api_connection.src.api_connector import APIDataConnector
+from app.api_connection.src.get_api_data import APIError
+
+from app.schemas import *
+from app.crud import *
+
+import logging
+
+
+API_BASE = f'https://ll.thespacedevs.com/2.3.0/launches/?limit=50&ordering=net&net__gte={datetime.datetime.now().isoformat()}&format=json&mode=list'
+
+
+async def get_api_data(db):
+    current_launches = await get_current_launches(db)
+    connector = APIDataConnector(API_BASE, current_launches, max_loop_count=20)
+    try:
+        data = await asyncio.to_thread(connector.get_difference)
+        for flight_data in data:
+            flight_data.rocket['agency_id'] = flight_data.agency.get('id')
+            flight_data.launch['site_id'] = flight_data.site.get('id') if flight_data.site is not None else None
+            flight_data.launch['program_id'] = flight_data.program.get('id') if flight_data.program is not None else None
+            flight_data.launch['rocket_id'] = flight_data.rocket.get('id')
+            
+            if flight_data.site is not None:
+                site_data = SiteCreate(**flight_data.site)
+                if await update_site(db=db, site_id=flight_data.site.get('id'), site_data=site_data) is None:
+                    await create_site(db=db, site_data=site_data)
+                    logging.info(f'Added site - {flight_data.site.get('id')}')
+                    
+            if flight_data.program is not None:
+                program_data = ProgramCreate(**flight_data.program)
+                if await update_program(db=db, program_id=flight_data.program.get('id'), program_data=program_data) is None:
+                    await create_program(db=db, program_data=program_data)
+                    logging.info(f'Added program - {flight_data.program.get('id')}')
+
+            agency_data = AgencyCreate(**flight_data.agency)
+            if await update_agency(db=db, agency_id=flight_data.agency.get('id'), agency_data=agency_data) is None:
+                await create_agency(db=db, agency_data=agency_data)    
+                logging.info(f'Added agency - {flight_data.agency.get('id')}')        
+            
+            rocket_data = RocketCreate(**flight_data.rocket)
+            if await update_rocket(db=db, rocket_id=flight_data.rocket.get('id'), rocket_data=rocket_data) is None:
+                await create_rocket(db=db, rocket_data=rocket_data)
+                logging.info(f'Added rocket - {flight_data.rocket.get('id')}')
+            
+            launch_data = LaunchCreate(**flight_data.launch)
+            if await update_launch(db=db, launch_id=flight_data.launch.get('id'), launch_data=launch_data) is None:
+                await create_launch(db=db, launch_data=launch_data)
+                logging.info(f'Added launch - {flight_data.launch.get('id')}')
+
+    except APIError as e:
+        logging.warning(f'Api error occurred: {e}')
--- blastoff_app/app/lifespan.py
+++ blastoff_app/app/lifespan.py
@@ -1,11 +1,20 @@
 from contextlib import asynccontextmanager
 from datetime import timedelta
+import asyncio
 
 from fastapi import FastAPI
 
-from app.database import Base, SessionLocal, engine
+from app.database import Base, SessionLocal, engine, get_db
 from app.emails import EmailNotifier
 from app.redis import redis
+from app.get_data import get_api_data
+
+
+async def scheduled_task():
+    while True:
+        async for db in get_db():
+            await get_api_data(db)
+        await asyncio.sleep(3600)
 
 
 @asynccontextmanager
@@ -13,7 +22,7 @@ async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     await redis.init_connection()
-
+    asyncio.create_task(scheduled_task())
     async with SessionLocal() as db:
         email_notifier = EmailNotifier(app, db, redis)
         await email_notifier.schedule_notifications(
