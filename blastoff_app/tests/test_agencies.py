ff1459a99da5148a26213bcdf84b102dce5aa766
lukassw1 <lukwoj27@gmail.com>
2025-01-07 03:34:59 +0100

    tests for agencies and useres routers

---
+++ blastoff_app/tests/test_agencies.py
@@ -0,0 +1,148 @@
+import pytest
+import httpx
+from hypothesis import given, settings
+from hypothesis.strategies import integers, text
+
+URL_HOST = "http://localhost:8000/api/v1/agencies/"
+TEST_AGENCY_ID = 501
+
+
+@given(
+    name=text(min_size=1, max_size=50),
+    country=text(min_size=1, max_size=50),
+    description=text(min_size=1, max_size=100),
+    website=text(min_size=1, max_size=100),
+    image_url=text(min_size=1, max_size=100),
+)
+@settings(max_examples=10)
+@pytest.mark.asyncio
+async def test_create_and_delete_agency(
+    name, country, description, website, image_url
+):
+    agency_id = TEST_AGENCY_ID
+    agency_data = {
+        "id": agency_id,
+        "name": name,
+        "country": country,
+        "description": description,
+        "website": website,
+        "image_url": image_url,
+    }
+
+    async with httpx.AsyncClient() as client:
+        create_response = await client.post(URL_HOST, json=agency_data)
+    assert create_response.status_code == 200
+    created_agency = create_response.json()
+    assert created_agency["id"] == agency_data["id"]
+    assert created_agency["name"] == agency_data["name"]
+    assert created_agency["country"] == agency_data["country"]
+    assert created_agency["description"] == agency_data["description"]
+    assert created_agency["website"] == agency_data["website"]
+    assert created_agency["image_url"] == agency_data["image_url"]
+
+    async with httpx.AsyncClient() as client:
+        delete_response = await client.delete(URL_HOST + f"{agency_id}")
+    assert delete_response.status_code == 200
+    deleted_agency = delete_response.json()
+    assert deleted_agency["id"] == agency_id
+    assert deleted_agency["name"] == agency_data["name"]
+
+    async with httpx.AsyncClient() as client:
+        get_response = await client.get(URL_HOST + f"{agency_id}")
+    assert get_response.status_code == 404
+    response_data = get_response.json()
+    assert response_data["detail"] == "Agency not found"
+
+
+async def test_create_agency(monkeypatch):
+    agency_data = {
+        "id": TEST_AGENCY_ID,
+        "name": "SpaceX",
+        "country": "USA",
+        "description": "Private space exploration company.",
+        "website": "https://www.spacex.com",
+        "image_url": "https://www.spacex.com/logo.png",
+    }
+    async with httpx.AsyncClient() as client:
+        r = await client.post(URL_HOST, json=agency_data)
+    assert r.status_code == 200
+    response_data = r.json()
+    assert response_data["id"] == agency_data["id"]
+    assert response_data["name"] == agency_data["name"]
+    assert response_data["country"] == agency_data["country"]
+    assert response_data["description"] == agency_data["description"]
+    assert response_data["website"] == agency_data["website"]
+    assert response_data["image_url"] == agency_data["image_url"]
+
+
+async def test_update_agency(monkeypatch):
+    updated_agency_data = {
+        "name": "string",
+        "country": "string",
+        "description": "string",
+        "website": "string",
+        "image_url": "string",
+        "id": 501,
+    }
+    async with httpx.AsyncClient() as client:
+        r = await client.put(
+            URL_HOST + f"{TEST_AGENCY_ID}", json=updated_agency_data
+        )
+    assert r.status_code == 200
+    response_data = r.json()
+    assert response_data["id"] == updated_agency_data["id"]
+    assert response_data["name"] == updated_agency_data["name"]
+    assert response_data["country"] == updated_agency_data["country"]
+    assert response_data["description"] == updated_agency_data["description"]
+    assert response_data["website"] == updated_agency_data["website"]
+    assert response_data["image_url"] == updated_agency_data["image_url"]
+
+
+async def test_get_agency(monkeypatch):
+    updated_agency_data = {
+        "name": "string",
+        "country": "string",
+        "description": "string",
+        "website": "string",
+        "image_url": "string",
+        "id": 501,
+    }
+    async with httpx.AsyncClient() as client:
+        r = await client.get(URL_HOST + f"{TEST_AGENCY_ID}")
+    assert r.status_code == 200
+    response_data = r.json()
+    assert response_data["id"] == updated_agency_data["id"]
+    assert response_data["name"] == updated_agency_data["name"]
+    assert response_data["country"] == updated_agency_data["country"]
+    assert response_data["description"] == updated_agency_data["description"]
+    assert response_data["website"] == updated_agency_data["website"]
+    assert response_data["image_url"] == updated_agency_data["image_url"]
+
+
+async def test_delete_agency(monkeypatch):
+    updated_agency_data = {
+        "name": "string",
+        "country": "string",
+        "description": "string",
+        "website": "string",
+        "image_url": "string",
+        "id": 501,
+    }
+    async with httpx.AsyncClient() as client:
+        r = await client.delete(URL_HOST + f"{TEST_AGENCY_ID}")
+    assert r.status_code == 200
+    response_data = r.json()
+    assert response_data["id"] == updated_agency_data["id"]
+    assert response_data["name"] == updated_agency_data["name"]
+    assert response_data["country"] == updated_agency_data["country"]
+    assert response_data["description"] == updated_agency_data["description"]
+    assert response_data["website"] == updated_agency_data["website"]
+    assert response_data["image_url"] == updated_agency_data["image_url"]
+
+
+async def test_delete_wrong_id_agency(monkeypatch):
+    async with httpx.AsyncClient() as client:
+        r = await client.delete(URL_HOST + f"{TEST_AGENCY_ID+1}")
+    assert r.status_code == 404
+    response_data = r.json()
+    assert response_data["detail"] == "Agency not found"
--- blastoff_app/tests/test_users.py
+++ blastoff_app/tests/test_users.py
@@ -1,71 +1,40 @@
-from datetime import datetime
+import pytest
+import httpx
+from hypothesis import given, settings
+from hypothesis.strategies import text, characters
 
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_create_user():
-    unique_name = f"username_{datetime.now().timestamp()}"
-    # Create a new user with a unique username
-    response = client.post(
-        "/api/v1/users/register/",
-        json={
-            "username": unique_name,
-            "email": f"{unique_name}@example.com",
-            "password": "1234",
-        },
-    )
-    assert response.status_code == 200
-    data = response.json()
-    assert data["username"] == unique_name
-    assert data["email"] == f"{unique_name}@example.com"
-    assert "id" in data
+URL_HOST = "http://localhost:8000/api/v1/users/"
+allowed_characters = characters(blacklist_categories=["Cc", "Cs"])
 
 
-"""
+@given(
+    username=text(min_size=5, max_size=20, alphabet=allowed_characters),
+    email=text(min_size=5, max_size=30, alphabet=allowed_characters),
+    password=text(min_size=5, max_size=20, alphabet=allowed_characters),
+)
+@settings(max_examples=10, deadline=500)
+@pytest.mark.asyncio
+async def test_create_user(username, email, password):
+    user_data = {"username": username, "email": email, "password": password}
 
-def test_get_user():
-    user_id = _test_create_user()  # Create user and get the user ID
-    unique_name = f"username_{datetime.now().timestamp()}"
-    # Retrieve the created user
-    response = client.get(f"/users/{user_id}")
-    assert response.status_code == 200
-    data = response.json()
-    assert data["id"] == user_id
-    assert data["username"] == unique_name  # Ensure the username is correct
-    assert data["email"] == f"{unique_name}@example.com"  # Ensure the email is correct
+    async with httpx.AsyncClient() as client:
+        r = await client.post(URL_HOST + "register", json=user_data)
+    assert r.status_code == 200
+    response_data = r.json()
+    test_user_id = response_data["id"]
+    assert response_data["username"] == user_data["username"]
+    assert response_data["email"] == user_data["email"]
+    with pytest.raises(KeyError):
+        _ = response_data["password"]
 
-def test_update_user_email():
-    user_id = _test_create_user()  # Create user and get the user ID
-    unique_name = f"username_{datetime.now().timestamp()}"
-    new_email = f"newemail_{datetime.now().timestamp()}@example.com"
-    # Update the user's email address
-    response = client.put(
-        f"/users/{user_id}",
-        json={"email": new_email},
-    )
-    assert response.status_code == 200
-    data = response.json()
-    assert data["id"] == user_id
-    assert data["email"] == new_email
+    async with httpx.AsyncClient() as client:
+        r = await client.delete(URL_HOST + f"{test_user_id}")
+    assert r.status_code == 200
+    response_data = r.json()
+    assert response_data["message"] == "User deleted successfully"
 
-    # Verify the change by getting the updated user
-    get_response = client.get(f"/users/{user_id}")
-    assert get_response.status_code == 200
-    updated_data = get_response.json()
-    assert updated_data["email"] == new_email
-
-def test_delete_user():
-    user_id = _test_create_user()  # Create user and get the user ID
-    # Delete the created user
-    response = client.delete(f"/users/{user_id}")
-    assert response.status_code == 200
-    assert response.json() == {"message": "User deleted successfully"}
-
-    # Try to retrieve the deleted user
-    get_response = client.get(f"/users/{user_id}")
-    assert get_response.status_code == 404  # User should be deleted, so return 404
-"""
+    async with httpx.AsyncClient() as client:
+        r = await client.delete(URL_HOST + f"{test_user_id}")
+    assert r.status_code == 404
+    response_data = r.json()
+    assert response_data["detail"] == "User not found"
